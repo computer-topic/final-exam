3e897b8a12f67e676fc435f01eeafff352976b15
dpfls0922 <dpfls0922@naver.com>
2024-12-07 22:36:23 +0900

    docs: chapter 09 코드 셋팅

---
+++ chapter09/euna/9-1.java
@@ -0,0 +1,18 @@
+public class Calc {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+}
+
+// Calc 클래스에 대한 testcode 작성하라
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+
+class CalcTest {
+
+}
